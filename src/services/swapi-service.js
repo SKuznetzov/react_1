2d2a80ccc298dc5cf9820a6a361b9b6ee31e083f
SKuznetzov <s-kuznecov100@rambler.ru>
2021-12-14 15:47:03 +0300

    add getResource()

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,3 +1,17 @@
 export default class SwapiService {
-    
+
+    async getResource() {
+        const res = await fetch(url)
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` + `,received ${res.status}`)
+        }
+        return await res.json()
+    }
+    getAllPeople() {
+        return this.getResource(`https://swapi.dev/api/people/`)
+    }
+    getPerson(id) {
+        return this.getResource(`https://swapi.dev/api/people/${id}/`)
+    }
 }
