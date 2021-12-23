7937880ff886625168c07b4bea9b7822b83dd30c
SKuznetzov <s-kuznecov100@rambler.ru>
2021-12-23 16:22:17 +0300

    import components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import Header from '../header'
+import RandomPlanet from '../random-planet'
+import ItemList from '../item-list'
+import PersonDetails from '../person-details'
+
 import './app.css'
 
 
@@ -6,6 +11,15 @@ const App = () => {
     return (
         <div>
             <Header />
+            <RandomPlanet />
+            <div className="row md2">
+                <div className="col-md-6">
+                    <ItemList />
+                </div>
+                <div className="col-md-6">
+                    <PersonDetails />
+                </div>
+            </div>
         </div>
     )
 }
