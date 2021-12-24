852c70c7c6af13642b92c9623a1bf179f25fe22f
SKuznetzov <s-kuznecov100@rambler.ru>
2021-12-24 20:10:52 +0300

    state elements

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,11 +4,17 @@ import './random-planet.css'
 export default class RandomPlanet extends Component {
 
     state = {
+        name:null,
         population: null,
         rotationPeriod: null,
         diameter: null
     }
+
+    updatePlanet() {
+
+    }
     render() {
+        const {planet:{id,name,population,rotationPeriod,diameter} }= this.state;
         return (
             <div className="random-planet jumbotron rounded">
             <img className="planet-image"
@@ -17,16 +23,16 @@ export default class RandomPlanet extends Component {
                 <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
                      <li className="list-group-item">
-                         <span className="term">{population}</span>
-                         <span>123456</span>
+                         <span className="term">Population</span>
+                         <span>{population}</span>
                      </li>
                      <li className="list-group-item">
-                         <span className="term">{rotationPeriod}</span>
-                         <span>43</span>
+                         <span className="term">RotationPeriod</span>
+                         <span>{rotationPeriod}</span>
                      </li>
                      <li className="list-group-item">
-                         <span className="term">{diameter}</span>
-                         <span>100</span>
+                         <span className="term">Diameter</span>
+                         <span>{diameter}</span>
                      </li>
                 </ul>
             </div>
