42aa892fcbc3e6f09f2609fa069959d6976847da
SKuznetzov <s-kuznecov100@rambler.ru>
2021-12-26 19:26:37 +0300

    add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Spinner = () => {
+    return (
+        <div className="lds-css">
+            <div className="lds-double-ring">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
