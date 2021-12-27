3941379ad0281903521aa437ad66178b21918569
SKuznetzov <s-kuznecov100@rambler.ru>
2021-12-27 20:48:41 +0300

    import css

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './spinner.css'
 export const Spinner = () => {
     return (
 <div class="loadingio-spinner-double-ring-yr9t2opulw"><div class="ldio-ftfm0uwzfrf">
