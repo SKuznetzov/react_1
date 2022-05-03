6f7da320f1322ed3d774e3d5d1907d70f930be1e
SKuznetzov <s-kuznecov100@rambler.ru>
2022-05-03 16:26:56 +0300

    add ErrorIndicator

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const error-indicator = () => {
+const ErrorIndicator = () => {
     return (
         <div>
-            
+            <span className="error-indicator">
+                <span>something has gone terribly wrong</span>
+                <span>(but we already sent droids to fix it)</span>
+            </span>
         </div>
     )
 }
