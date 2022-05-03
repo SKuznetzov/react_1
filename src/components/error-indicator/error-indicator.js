f879f1ca4bedcfe0a02a9e72d401ac815bf68769
SKuznetzov <s-kuznecov100@rambler.ru>
2022-05-03 16:14:41 +0300

    add component error indicator

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class error-indicator extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
