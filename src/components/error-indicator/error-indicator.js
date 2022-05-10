61d8e99650f6f3325a5886c04717e51c828fc85d
SKuznetzov <s-kuznecov100@rambler.ru>
2022-05-10 17:03:52 +0300

    formation

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -4,10 +4,9 @@ import 'error-indicator.css'
 const ErrorIndicator = () => {
     return (
         <div className="error-indicator">
-                <span className="boom">BOOM!!!</span>
-                <span>something has gone terribly wrong</span>
-                <span>(but we already sent droids to fix it)</span>
-            
+            <span className="boom">BOOM!!!</span>
+            <span>something has gone terribly wrong</span>
+            <span>(but we already sent droids to fix it)</span>
         </div>
     )
 }
