27b10ebad5fbc06d6a9a9269e878bd52b1428092
SKuznetzov <s-kuznecov100@rambler.ru>
2022-05-11 19:34:17 +0300

    change updatePlabet

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -4,6 +4,7 @@ import './error-indicator.css'
 const ErrorIndicator = () => {
     return (
         <div className="error-indicator">
+            {/* <img src=   alt="" /> */}
             <span className="boom">BOOM!!!</span>
             <span>something has gone terribly wrong</span>
             <span>(but we already sent droids to fix it)</span>
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -30,7 +30,7 @@ export default class RandomPlanet extends Component {
         })
     }
 
-    updatePlanet() {
+    updatePlanet = () => {
         const id = Math.floor(Math.random()*25) + 2
       this.swapiService
       .getPlanet(id)
