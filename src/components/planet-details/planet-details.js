d591187d101c68b341b70132e8213305b3b344f5
SKuznetzov <s-kuznecov100@rambler.ru>
2022-05-31 11:37:49 +0300

    add component PlanetDetails

---
+++ src/components/planet-details/planet-details.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PlaneDetails extends Component {
+    render() {
+        return (
+            <div>
+                <h1>123</h1>
+            </div>
+        )
+    }
+}
