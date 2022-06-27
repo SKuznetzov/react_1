ca8d9a11053e29b6ac7606942d7c69397220e2f1
SKuznetzov <s-kuznecov100@rambler.ru>
2022-06-27 18:02:48 +0300

    component PeoplePage

---
+++ src/components/people-page/people-page.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PeoplePage extends Component {
+    render() {
+        return (
+            <div>
+                <p>People page</p>
+            </div>
+        )
+    }
+}
