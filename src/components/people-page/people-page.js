661d11e73459d9a86f2777a8382da500275dcaa2
SKuznetzov <s-kuznecov100@rambler.ru>
2022-06-27 18:07:00 +0300

    function onPersonSelected

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class PeoplePage extends Component {
+    
+    onPersonSelected = (id) => {
+        this.setState({
+            selectedPerson: id
+        })
+    }
     render() {
         return (
             <div>
