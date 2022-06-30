af57b339a13080829bb4b59be71714185a487e5d
SKuznetzov <s-kuznecov100@rambler.ru>
2022-06-30 18:32:39 +0300

    delete import

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,6 @@
 import React,{Component} from 'react'
 import Header from '../header'
 import RandomPlanet from '../random-planet'
-import ItemList from '../item-list'
-import PersonDetails from '../person-details'
 import ErrorIndicator from '../error-indicator'
 import './app.css'
 
